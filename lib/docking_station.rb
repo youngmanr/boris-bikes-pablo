626869277d51affe8d77d0558e25960889488c0d
Tom Barnes <ruralmyth@gmail.com>
2015-10-13 16:43:52 +0100

    completed part 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,9 @@
+require_relative 'bike'
+
 class DockingStation
 
   def release_bike
+    bike = Bike.new
   end
-  
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,33 @@
 require 'docking_station'
 
 describe DockingStation do
+  
+  before do 
+    @bike = subject.release_bike
+    @working = @bike.working?
+  end
+
   it { is_expected.to respond_to :release_bike }
+
+  it "should create a new instance of bike when asked to release_bike" do
+    (expect(@bike).to be_a Bike) && (expect(@working).to eq true)
+  end
+
 end
+
+
+
+
+
+=begin
+require 'docking_station'
+
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+
+  it "should create a new instance of bike when asked to release_bike" do
+    (expect(subject.release_bike).to be_a Bike) && (expect(subject.release_bike.working?).to eq true)
+  end
+
+end
+=end
