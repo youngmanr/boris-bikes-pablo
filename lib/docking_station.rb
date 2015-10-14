1caa5437772c5c5becc236931511d591bda8f684
Andrew Dowell <adowellphoto@gmail.com>
2015-10-14 14:27:47 +0100

    replaced original answers with 12 solution

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,30 +2,19 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bike, :bikes
+  attr_reader :bike
 
   def initialize
     @bikes = []
   end
 
   def release_bike
-    if @bikes.length >= 10 #assume 10 bikes are docked when a DockingStation instance is created
-      fail "No bikes available"
-    else
-      new_bike = Bike.new
-      @bikes << new_bike
-    end
-    new_bike
+    fail 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
+    fail 'Already full' if @bike
     @bike = bike
   end
-
 end
-
-# p station = DockingStation.new
-# p bike = station.release_bike
-# p bike.working?
-# p station.dock(bike)
-# p station.bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,59 +1,26 @@
 require 'docking_station'
 
 describe DockingStation do
-  #method here??
-  bike_overflow = DockingStation.new
-  10.times do
-    bike_overflow.release_bike
-  end
 
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 
   it { is_expected.to respond_to(:dock).with(1) }
   it { is_expected.to respond_to(:bike) }
 
-  it 'raises error on no bikes available' do
-    expect {bike_overflow.release_bike}.to raise_error("No bikes available")
+  it 'raises error when there are no more bikes available' do
+    expect {subject.release_bike}.to raise_error("No bikes available")
   end
+
+  # it 'refuses to dock bike when already at capacity' do
+  #   expect {subject.dock(bike)}.to raise_error("Already full")
+  # end
 
 end
-
-
-
-=begin
-____________________________________________________________________________________________________
-require 'docking_station'
-
-describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
-
-  it "should create a new instance of bike when asked to release_bike" do
-    (expect(subject.release_bike).to be_a Bike) && (expect(subject.release_bike.working?).to eq true)
-  end
-
-end
-=end
-
-=begin
-____________________________________________________________________________________________________
-require 'docking_station'
-
-describe DockingStation do
-
-  before do
-    @bike = subject.release_bike
-    @working = @bike.working?
-  end
-
-  it { is_expected.to respond_to :release_bike }
-
-  it "should create a new instance of bike when asked to release_bike" do
-    (expect(@bike).to be_a Bike) && (expect(@working).to eq true)
-  end
-end
-=end
